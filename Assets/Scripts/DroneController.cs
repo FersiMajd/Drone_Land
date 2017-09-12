090d8ebb00ca96c62b9dbb96a104e7f59052c0f4
Majd Fersi <fersi.majd@gmail.com>
2017-09-12 23:54:49 +0100

    deone script

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -2,16 +2,20 @@
 
 public class DroneController : MonoBehaviour
 {
+    public float DroneMaxAltitude, FlySpeed, RotationSpeed;
 
-    // Use this for initialization
-    void Start()
-    {
+    private float _verticalAxis, _horizontalAxis;
+    private Vector3 _velocity = Vector3.zero;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        _verticalAxis = Input.GetAxis("Vertical") * Time.deltaTime * FlySpeed;
+        _horizontalAxis = Input.GetAxis("Horizontal") * Time.deltaTime * RotationSpeed;
 
+        transform.position += Vector3.up * _verticalAxis;
+        transform.Rotate(Vector3.up, _horizontalAxis);
+
+        Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, transform.position + Vector3.one, ref _velocity, 10);
+        Camera.main.transform.LookAt(transform.position);
     }
 }
